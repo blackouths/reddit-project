a6a18facc4a26d83bf2815736beb3ab73a89ba22
sawaizmirza <m_sawaiz1791@ymail.com>
2019-08-14 13:36:08 +0100

    Popular DropDown

---
+++ src/components/Navbar/Dropdown/Dropdown.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Dropdown.css'
+
+
+class Dropdown extends React.Component {
+    state = {
+        items: this.props.items,
+        showItems: false
+    }
+
+    render() {
+        return <div>
+            <div className='select-box-arrow'>
+                
+                <span className={`${this.state.showItems ? 'select-box--arrow-up' : 'select-box--arrow-down'}`} />
+                </div>
+           
+            {
+                this.state.items.map(item => <div key={item.id}>
+                    {item.value}
+                </div>)
+            }
+        }
+        </div>
+}}
+
+export default Dropdown;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import '../SideDrawer/DrawerToggleButton';
+import './SideDrawer/DrawerToggleButton';
 import './Navbar.css';
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import DrawerToggleButton from './SideDrawer/DrawerToggleButton';
+import Dropdown from './Dropdown/Dropdown';
 
 
 
 const Toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">>
-            <div>
-            <DrawerToggleButton/>
-            </div>
+            <div><DrawerToggleButton/></div>
             <div className="toolbar_logo"><a href = "/">THE LOGO</a></div>
+            <Dropdown
+                items={[
+                    {value: 'Popular', id: 1},
+                    {value: 'All', id: 2},
+                    {value: 'Original Content', id: 3},
+                    {value: 'Top Communities', id: 4},
+                    {value: 'Coins', id: 5},
+                    {value: 'Premium', id: 6},
+                ]} />
             <div className="spacer"/>
             <div className="toolbar_navigation_items">
              <ul>
