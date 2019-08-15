e4fa0df2b3bb71ddd1f60b061f48f49e261143aa
blackouths <lspencerea@gmail.com>
2019-08-15 17:39:33 +0100

    Installed bootstrap, fetched reddit background image
    Created Content box and fetched reddit bg image.

---
+++ src/components/contentBox/contentbox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ContentBox = props => {
+    return (
+        <div>
+            <ul>
+                <div className="background-banner">
+                <h2><span>Today's Top Growing Communities</span></h2>
+                </div>
+                </ul>   
+            </div>
+
+        
+        
+    );
+}
+export default ContentBox;
