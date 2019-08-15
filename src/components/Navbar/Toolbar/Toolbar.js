2888b3b5468cb58a705ba3895794a8bcc4df50ea
rJd93 <katarina_khy@outlook.com>
2019-08-15 11:16:08 +0100

    hkjhk

--- src/components/Navbar/Toolbar/Toolbar.js
+++ src/components/Navbar/Toolbar/Toolbar.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Dropdown from '../Dropdown/Dropdown';
-
-
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 
@@ -10,20 +7,6 @@ const toolbar = props => (
     <nav className="toolbar__navigation">
         <div className="toolbar__toggle-button">
             <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-      <div className='dropdownMenu'> <div style= {{ margin: '16px', position: 'relative'}}> 
-        <Dropdown items = {[
-            {value: 'Popular', id:1},
-            {value: 'All', id:2},
-            {value: 'Original Content', id:3},
-            {value: 'Top Communities', id:4},
-            {value: 'Coins', id:5},
-            {value: 'Premium', id:6}
-
-          ]} >
-
-          </Dropdown> 
-        </div>
         </div>
         <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
         <div className="spacer" />
