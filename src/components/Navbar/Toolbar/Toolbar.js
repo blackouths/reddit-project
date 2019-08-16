20e84266948a7b4de650130f5866379d55642a44
rJd93 <katarina_khy@outlook.com>
2019-08-16 10:56:03 +0100

    New

--- src/components/Navbar/Toolbar/Toolbar.js
+++ src/components/Navbar/Toolbar/Toolbar.js
@@ -4,6 +4,13 @@ import './Toolbar.css';
 import Reddit from '../icons/Reddit.png'
 import SearchBar from '../SearchBar/SearchBar'
 import Dropdown from '../Dropdown/Dropdown'
+import Popular from '../icons/popular.png'
+import all from '../icons/all.png'
+import oc from '../icons/oc.png'
+import communities from '../icons/communities.png'
+import coin from '../icons/coin.png'
+import premium from '../icons/premium.png'
+import User from '../User/User'
 
 const toolbar = props => (
   <header className="toolbar">
@@ -14,12 +21,12 @@ const toolbar = props => (
         <div className="toolbar__logo"><a href="/"><img src={Reddit} alt="reddit icon"/></a></div>
         <div className='dropdownMenu'> <div style= {{ margin: '16px', position: 'relative'}}>
        <Dropdown items = {[
-           {value: 'Popular', id:1},
-           {value: 'All', id:2},
-           {value: 'Original Content', id:3},
-           {value: 'Top Communities', id:4},
-           {value: 'Coins', id:5},
-           {value: 'Premium', id:6}
+           {value:  <span className='dd'><img className='pic' src= {Popular} alt="icon" />Popular</span>, id:1},
+           {value:  <span className='dd'><img className='pic' src= {all} alt="icon" />All</span>, id:2},
+           {value:  <span className='dd'><img className='pic' src= {oc} alt="icon"/>Original Content</span> , id:3},
+           {value: <span className='dd'><img className='spic' src= {communities} alt="icon"/>Top Communities</span>, id:4},
+           {value: <span className='dd'><img className='spic' src= {coin} alt="icon"/>Coins</span>, id:5},
+           {value: <span className='dd'><img className='spic' src= {premium} alt="icon"/>Premium</span>, id:6}
 
          ]} >
 
@@ -30,6 +37,16 @@ const toolbar = props => (
         <SearchBar searchBoxName={"userNameSearch"} onSearchTermChange={this.onSearch}/>
         <div className="spacer" />
         <div className="toolbar_navigation-items">
+        <div className='user'>
+        <User items = {[
+          {value: 'Night Mode', id:1 },
+          {value: 'Reddit Coins', id:2 },
+          {value: 'Reddit Premium', id:3 },
+          {value: 'Help Center', id:4 },
+          {value: 'Log in/Sign Up', id:5 },
+        ]}> </User>
+
+        </div>
         </div>
     </nav>
   </header>
