9b0582d6a00484df617b6a58a0aca57e22a85aa9
blackouths <lspencerea@gmail.com>
2019-08-19 11:38:36 +0100

    hidden content box for time being

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
         <SideDrawer show={this.state.sideDrawerOpen} />
         {backdrop}
         <main style={{marginTop: '54px'}}>
-        <ContentBox/>
+        {/* <ContentBox/> */}
         <br/>
         <Wide ratings= '24.6K'  field= 'coding' info='I have dedicated myself to the #100daysofcoding challenge and have started to do live streams everyday on Twitch. Link is down below. Will probably start around 10pm BST depending on how long my D and D game goes on for.' comments='53 Comments' shares='Share'/>
         <br/>
